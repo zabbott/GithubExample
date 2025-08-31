0a9e29c4092f9b52e5d8af13c690ccedb05d242b
zabbott <zabbott904@gmail.com>
2025-08-31 17:38:36 -0500

    update

---
+++ Assets/TheBestScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class TheBestScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("hello world you wild world you");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
