78616a5c5fdd2526dfe6d626e293f579afa1eb03
zabbott <zabbott904@gmail.com>
2025-08-31 17:39:20 -0500

    Update TheBestScript.cs

--- Assets/TheBestScript.cs
+++ Assets/TheBestScript.cs
@@ -5,7 +5,7 @@ public class TheBestScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        print("hello world you wild world you");
+        print("HOLY SHIT BATMAN WE'RE IN 289");
     }
 
     // Update is called once per frame
